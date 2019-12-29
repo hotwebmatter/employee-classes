ca21d701a3416b31f38d51c110ed87be4005c513
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-29 10:58:53 -0500

    Auto-implement public property members

--- Employee.cs
+++ Employee.cs
@@ -6,5 +6,21 @@ namespace employee_classes
 {
     class Employee
     {
+        // data members
+        private int employeeNumber;
+        private string firstName,
+                       lastName,
+                       jobDescription,
+                       department;
+        private DateTime dateOfHire;
+        private double monthlySalary;
+        // auto-implemented property members
+        public int EmployeeNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobDescription { get; set; }
+        public string Department { get; set; }
+        public DateTime DateOfHire { get; set; }
+        public double MonthlySalary { get; set; }
     }
 }
