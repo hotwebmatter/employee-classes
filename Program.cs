f288cb1a6478a08322f20036baceb87030ce17df
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-29 10:48:26 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace employee_classes
 {
@@ -6,7 +7,7 @@ namespace employee_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
